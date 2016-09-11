b85455c9d7a1ca5dbc9ae9342ad86b219fb9d56e
ostrbor <ostrbor@gmail.com>
2016-09-11 18:37:16 +0300

    Style improvements
    Made according to pep8.

--- autoproject.py
+++ autoproject.py
@@ -1,9 +1,11 @@
-import json
-import requests
+"""Create project folder, github repo with
+content from Templates"""
 from pathlib import Path
+import json
 import subprocess
 import os
-from config import USER, GITHUB_TOKEN
+from config import GITHUB_USER, GITHUB_TOKEN
+import requests
 
 TEMPLATE_DIR = "Templates"
 PROJECT_HOME_ENV = "PROJECT_HOME"
@@ -11,14 +13,13 @@ MAKE_VENV_CMD = "source /usr/local/bin/virtualenvwrapper.sh && mkproject {}"
 COPY_CMD = "cp -R {SRC}/* {DEST}"
 GIT_INIT_CMD = "cd {DEST} && git init && git add . && git commit -m 'First commit'"
 GITHUB_API_URL = "https://api.github.com/user/repos?access_token={TOKEN}"
-GITHUB_PUSH_CMD = "cd {DEST} && git remote add origin git@github.com:{USER}/{REPOS}.git "\
+GITHUB_PUSH_CMD = "cd {DEST} && git remote add origin git@github.com:{GITHUB_USER}/{REPOS}.git "\
                   "&& git push origin master"
 
 
 def shell_cmd(cmd):
-    res = subprocess.run(cmd, shell=True, executable='/bin/zsh')
-    if res.returncode != 0:
-        raise Exception("Couldn't execute shell command.", cmd)
+    "run shell command"
+    subprocess.run(cmd, check=True, shell=True, executable='/bin/zsh')
 
 if __name__ == '__main__':
     NAME = REPOS = input("Enter project name: ")
@@ -29,9 +30,11 @@ if __name__ == '__main__':
     shell_cmd(COPY_CMD.format(SRC=SRC, DEST=DEST))
     shell_cmd(GIT_INIT_CMD.format(DEST=DEST))
     DATA_JSON = json.dumps({"name": NAME})
-    res = requests.post(GITHUB_API_URL.format(TOKEN=GITHUB_TOKEN),
+    RES = requests.post(GITHUB_API_URL.format(TOKEN=GITHUB_TOKEN),
                         data=DATA_JSON)
-    if res.status_code != 201:
+    if RES.status_code != 201:
         print("Request to create repos failed!")
     # add SSH to github account
-    shell_cmd(GITHUB_PUSH_CMD.format(DEST=DEST, USER=USER, REPOS=REPOS))
+    shell_cmd(GITHUB_PUSH_CMD.format(DEST=DEST,
+                                     GITHUB_USER=GITHUB_USER,
+                                     REPOS=REPOS))
